6fe4b746739f89f06af44fef16f38b5f4d4c93c6
ashy404 <thony1124@gmail.com>
2026-04-17 17:42:02 +0200

    actualizacion de datos

---
+++ WebApplicationApiSimex/Models/SeguimientoOfertaDTO.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApplicationApiSimex.Models
+{
+    public class SeguimientoOfertaDTO
+    {
+
+        public int OfertaId { get; set; }
+        public string IncotermNombre { get; set; }
+        // Aquí incluimos la lista de pasos ya cruzada con su estado actual
+        public List<PasoSeguimientoDTO> Pasos { get; set; }
+
+    }
+
+    public class PasoSeguimientoDTO
+    {
+        public int TrackingStepId { get; set; }
+        public string NombrePaso { get; set; }
+        public int Orden { get; set; }
+        public int? EstadoActualId { get; set; } // 1: Pendiente, 2: En curso, 3: Finalizado
+    }
+}
